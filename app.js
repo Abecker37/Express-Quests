502e10ef7d407ead113d031959355a8c735fbe39
Abecker37 <72383676+Abecker37@users.noreply.github.com>
2023-10-10 10:16:55 +0200

    quete4

--- app.js
+++ app.js
@@ -17,7 +17,10 @@ app.get("/api/movies/:id", movieHandlers.getMovieById);
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
 app.post("/api/movies", movieHandlers.postMovie);
+app.put("/api/movies/:id", movieHandlers.updateMovie);
 app.post("/api/users", userHandlers.postUser);
+app.put("/api/users/:id", userHandlers.updateUsers);
+
 app.listen(port, (err) => {
   if (err) {
     console.error("Something bad happened");
