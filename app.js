6291172e671f94424e4a8a8dd3bddb221777b9fb
Abecker37 <72383676+Abecker37@users.noreply.github.com>
2023-10-10 10:40:56 +0200

    quete 5

--- app.js
+++ app.js
@@ -18,8 +18,10 @@ app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
 app.post("/api/movies", movieHandlers.postMovie);
 app.put("/api/movies/:id", movieHandlers.updateMovie);
+app.delete("/api/movies/:id", movieHandlers.deleteMovie);
 app.post("/api/users", userHandlers.postUser);
 app.put("/api/users/:id", userHandlers.updateUsers);
+app.delete("/api/users/:id", userHandlers.deleteUser);
 
 app.listen(port, (err) => {
   if (err) {
