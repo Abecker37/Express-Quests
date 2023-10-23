76311870a6e4386e6125b0184edb7ff5e9a47e08
Abecker37 <72383676+Abecker37@users.noreply.github.com>
2023-10-23 11:44:15 +0200

    express08

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const app = express();
 app.use(express.json());
 
 const port = process.env.APP_PORT ?? 8000;
-const { hashPassword } = require("./auth.js"); 
+const { hashPassword, verifyPassword, verifyToken } = require("./auth.js");
+const userHandlers = require("./userHandlers");
 const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
 };
@@ -15,18 +16,20 @@ const welcome = (req, res) => {
 app.get("/", welcome);
 
 const movieHandlers = require("./movieHandlers");
+app.post("/api/login", userHandlers.getUserByEmailWithPasswordAndPassToNext, verifyPassword);
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
-app.post("/api/movies", movieHandlers.postMovie);
-app.put("/api/movies/:id", movieHandlers.updateMovie);
-app.delete("/api/movies/:id", movieHandlers.deleteMovie);
-
-const userHandlers = require("./userHandlers");
 
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
 app.post("/api/users", hashPassword, userHandlers.postUser);
+
+app.use(verifyToken);
+app.post("/api/movies", movieHandlers.postMovie);
+app.put("/api/movies/:id", movieHandlers.updateMovie);
+app.delete("/api/movies/:id", movieHandlers.deleteMovie);
+
 app.put("/api/users/:id", hashPassword, userHandlers.updateUser);
 app.delete("/api/users/:id", userHandlers.deleteUser);
 
--- userHandlers.js
+++ userHandlers.js
@@ -112,18 +112,46 @@ const deleteUser = (req, res) => {
     });
 };
 
-module.exports = {
-  getUsers,
-  getUserById,
-  postUser,
-  updateUser,
-  deleteUser,
-};
+const getUserByEmailWithPasswordAndPassToNext = (req, res, next) => {
 
+  const { email } = req.body;
+
+
+  database
+
+    .query("select * from users where email = ?", [email])
+
+    .then(([users]) => {
+
+      if (users[0] != null) {
+
+        req.user = users[0];
+
+
+        next();
+
+      } else {
+
+        res.sendStatus(401);
+
+      }
+
+    })
+
+    .catch((err) => {
+
+      console.error(err);
+
+      res.status(500).send("Error retrieving data from database");
+
+    });
+
+};
 module.exports = {
     getUsers,
     getUserById,
     postUser,
     updateUser,
     deleteUser,
+    getUserByEmailWithPasswordAndPassToNext,
 };
